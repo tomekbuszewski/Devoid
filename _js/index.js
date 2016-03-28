91130848315f61ba5a707ce49c23592705ece717
Tomek Buszewski <tomek.buszewski@gmail.com>
2016-03-28 19:32:08 +0200

    Additional things for React

--- _js/index.js
+++ _js/index.js
@@ -49,14 +49,6 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-
-	__webpack_require__(2);
-
-/***/ },
-/* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
